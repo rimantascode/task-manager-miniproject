dfc91cde026caf87b29a5bd2fdae3c1ef5afb2d3
rimantascode <r.preiks@gmail.com>
2020-07-06 12:34:12 +0000

    Added form filds, and route add_task

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] ='mongodb+srv://RootUser87:RootUser87@myfirstcluser-enmor.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
@@ -14,6 +15,11 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
