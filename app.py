c379b19382212e261781fa3c4c6f8a28e316b6c3
rimantascode <r.preiks@gmail.com>
2020-07-07 03:03:50 +0000

    added select option and added initializer in base.html and aslo added a variable inn app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
@@ -6,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = 'mongodb+srv://RootUser87:RootUser87@myfirstcluser-enmor.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -14,12 +13,14 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html",
+                           tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
